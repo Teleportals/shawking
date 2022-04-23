76a1ca8a2abb3c99f2dca2d756aafb0b87887679
Tiago Neto <me@tiagofneto.com>
2022-04-23 12:26:31 +0200

    IConnext itnerface

---
+++ contracts/interfaces/IConnext.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+interface IConnext {
+  struct CallParams {
+    address to;
+    bytes callData;
+    uint32 originDomain;
+    uint32 destinationDomain;
+  }
+
+  struct XCallArgs {
+    CallParams params;
+    address transactingAssetId; // Could be adopted, local, or wrapped
+    uint256 amount;
+  }
+
+  function xcall(XCallArgs calldata _args) external payable returns (bytes32);
+}   

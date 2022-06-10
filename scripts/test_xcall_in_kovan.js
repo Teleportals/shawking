1bca9d88dfeaaec1507364da36f1beee92f1dbf8
DCota <32775237+DaigaroCota@users.noreply.github.com>
2022-06-09 20:34:51 -0400

    fixed script test_xcall_in_kovan.js

--- scripts/test_xcall_in_kovan.js
+++ scripts/test_xcall_in_kovan.js
@@ -4,17 +4,12 @@ const fs = require("fs");
 
 const { readDeployments, connextParams, testParams } = require("./utils");
 
-const CHAIN_A_NAME = 'rinkeby'; // initiation chain
 const CHAIN_B_NAME = "kovan"; // receiving chain
 
-const CHAIN_A_COLLATERAL_ASSET = testParams.testAssets.rinkeby.collateral;
 const CHAIN_B_COLLATERAL_ASSET = testParams.testAssets.kovan.collateral;
-
-const CHAIN_A_DEBT_ASSET = testParams.testAssets.rinkeby.debt;
 const CHAIN_B_DEBT_ASSET = testParams.testAssets.kovan.debt;
 
 const main = async () => {
-  const local = await readDeployments(CHAIN_A_NAME);
   const remote = await readDeployments(CHAIN_B_NAME);
 
   if (!process.env.TEST_USER_PKEY) {
@@ -22,7 +17,7 @@ const main = async () => {
   }
   const testUser = new hre.ethers.Wallet(process.env.TEST_USER_PKEY, hre.ethers.provider);
 
-  const teleporter = await hre.ethers.getContractAt("Teleporter", local.teleporter.address);
+  const teleporter = await hre.ethers.getContractAt("Teleporter", remote.teleporter.address);
 
   // execute xcall with Connext
   console.log(`...begin 'completeLoanTransfer()' xcall on ${CHAIN_B_NAME} teleporter`);
